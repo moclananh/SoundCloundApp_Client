49a356b5db6dd42cf6f27a4b2183158aee23530f
Thanh Check Merg <kristino5223@gmail.com>
2025-02-02 15:41:43 +0700

    feat: add time remandering and css wavesuffer

--- src/app/track/[slug]/page.tsx
+++ src/app/track/[slug]/page.tsx
@@ -1,21 +1,20 @@
-'use client'
-import WaveTrack from '@/components/track/wave.track';
-import { useSearchParams } from 'next/navigation'
-
+"use client";
+import WaveTrack from "@/components/track/wave.track";
+import { useSearchParams } from "next/navigation";
+import { Container } from "@mui/material";
 const DetailTrackPage = (props: any) => {
-    const { params } = props;
+  const { params } = props;
 
+  const searchParams = useSearchParams();
+  const search = searchParams.get("audio");
 
-    const searchParams = useSearchParams()
-    const search = searchParams.get('audio')
-
-    return (
-        <div>DetailTrackPage
-            <div>
-                <WaveTrack />
-            </div>
-        </div>
-    )
-}
+  return (
+    <Container>
+      <div>
+        <WaveTrack />
+      </div>
+    </Container>
+  );
+};
 
 export default DetailTrackPage;
--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,30 +1,118 @@
-'use client'
+"use client";
 
-import { useEffect, useRef } from "react";
-import WaveSurfer from "wavesurfer.js";
-import { useSearchParams } from 'next/navigation';
-
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { WaveSurferOptions } from "wavesurfer.js";
+import { useSearchParams } from "next/navigation";
+import { useWavesurfer } from "@/utils/customHook";
+import "./wave.scss";
 const WaveTrack = () => {
-    const searchParams = useSearchParams()
-    const fileName = searchParams.get('audio');
-    const containerRef = useRef<HTMLDivElement>(null);
+  const searchParams = useSearchParams();
+  const fileName = searchParams.get("audio");
+  const containerRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (containerRef.current) {
-            const wavesurfer = WaveSurfer.create({
-                container: containerRef.current,
-                waveColor: 'rgb(200, 0, 200)',
-                progressColor: 'rgb(100, 0, 100)',
-                url: `/api?audio=${fileName}`,
-            })
-        }
-    }, [])
+  const optionsMemo = useMemo((): Omit<WaveSurferOptions, "container"> => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d")!;
+    // Define the waveform gradient
+    const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
+    gradient.addColorStop(0, "#656666"); // Top color
+    gradient.addColorStop((canvas.height * 0.7) / canvas.height, "#656666"); // Top color
+    gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, "#ffffff"); // White line
+    gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, "#ffffff"); // White line
+    gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, "#B1B1B1"); // Bottom color
+    gradient.addColorStop(1, "#B1B1B1"); // Bottom color
+    // Define the progress gradient
+    const progressGradient = ctx.createLinearGradient(
+      0,
+      0,
+      0,
+      canvas.height * 1.35
+    );
+    progressGradient.addColorStop(0, "#EE772F"); // Top color
+    progressGradient.addColorStop(
+      (canvas.height * 0.7) / canvas.height,
+      "#EB4926"
+    ); // Top color
+    progressGradient.addColorStop(
+      (canvas.height * 0.7 + 1) / canvas.height,
+      "#ffffff"
+    ); // White line
+    progressGradient.addColorStop(
+      (canvas.height * 0.7 + 2) / canvas.height,
+      "#ffffff"
+    ); // White line
+    progressGradient.addColorStop(
+      (canvas.height * 0.7 + 3) / canvas.height,
+      "#F6B094"
+    ); // Bottom color
+    progressGradient.addColorStop(1, "#F6B094"); // Bottom color
 
-    return (
-        <div ref={containerRef}>
-            wave track
-        </div>
-    )
-}
+    return {
+      waveColor: gradient,
+      progressColor: progressGradient,
+      height: 150,
+      barWidth: 2,
+      url: `/api?audio=${fileName}`,
+    };
+  }, []);
+
+  const wavesurfer = useWavesurfer(containerRef, optionsMemo);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  const timeEl = document.querySelector("#time")!;
+  const durationEl = document.querySelector("#duration")!; //jquery
+
+  useEffect(() => {
+    if (!wavesurfer) return;
+    setIsPlaying(false);
+    const subscription = [
+      wavesurfer.on("play", () => {
+        setIsPlaying(true);
+      }),
+      wavesurfer.on("pause", () => {
+        setIsPlaying(false);
+      }),
+      wavesurfer.on(
+        "decode",
+        (duration) => (durationEl.textContent = formatTime(duration))
+      ),
+      wavesurfer.on(
+        "timeupdate",
+        (currentTime) => (timeEl.textContent = formatTime(currentTime))
+      ),
+    ];
+
+    return () => {
+      subscription.forEach((unsub) => unsub());
+    };
+  }, [wavesurfer]);
+
+  //onPlay button click
+  const onPlayClick = useCallback(() => {
+    if (wavesurfer) {
+      wavesurfer?.isPlaying() ? wavesurfer?.pause() : wavesurfer?.play();
+    }
+  }, [wavesurfer]);
+
+  //timeline update
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const secondsRemainder = Math.round(seconds % 60);
+    const paddedSeconds = `0${secondsRemainder}`.slice(-2);
+    return `${minutes}:${paddedSeconds}`;
+  };
+  return (
+    <div>
+      <div ref={containerRef} className="wave-form-container">
+        wave track
+        <div id="time">0:00</div>
+        <div id="duration">0:00</div>
+      </div>
+      <button onClick={() => onPlayClick()}>
+        {isPlaying ? "pause" : "play"}
+      </button>
+    </div>
+  );
+};
 
 export default WaveTrack;
